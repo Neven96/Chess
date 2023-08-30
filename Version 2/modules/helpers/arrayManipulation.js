8161c16bb20667af2b514bf4a3950e3dd84140c4
Even <even.holmen@hotmail.no>
2023-08-30 15:02:36 +0200

    Comments

--- Version 2/modules/helpers/arrayManipulation.js
+++ Version 2/modules/helpers/arrayManipulation.js
@@ -1,5 +1,6 @@
 import { myHeaders } from "./header.js";
 
+// Adds two equal length arrays together, item by item
 function arrayAddition(array1, array2) {
     if (array1.length !== array2.length) {
         return false;
@@ -7,6 +8,7 @@ function arrayAddition(array1, array2) {
     return array1.map((item, index) => item + array2[index]);
 }
 
+// Subtracts two equal length arrays from each other, item by item
 function arraySubtraction(array1, array2) {
     if (array1.length !== array2.length) {
         return false;
--- Version 2/modules/helpers/zeroPad.js
+++ Version 2/modules/helpers/zeroPad.js
@@ -1,5 +1,6 @@
 import { myHeaders } from "./header.js";
 
+// Adds zeros in front of a string to pad it correctly
 function zeroPad(length, num) {
     return num.toString().padStart(length, "0");
 }
