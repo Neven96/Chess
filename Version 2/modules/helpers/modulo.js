ee19a36d05e1b129635480cac575168626b6c146
Even <even.holmen@hotmail.no>
2023-09-27 13:05:54 +0200

    Added error handling to modulo module

--- Version 2/modules/helpers/modulo.js
+++ Version 2/modules/helpers/modulo.js
@@ -2,11 +2,14 @@ import { myHeaders } from "./header.js";
 
 // Proper modulo function for JavaScript
 /**
-* @param {number} [n] "Is the number that is to be checked"
-* @param {number} [m] "Is the number that is divided by"
+* @param {number} [a] "Is the number dividend"
+* @param {number} [n] "Is the number divisor"
 */
-function mod(n, m) {
-    return ((n % m) + m) % m;
+function mod(a, n) {
+    if (!Number.isInteger(a) || !Number.isInteger(n)) {
+        throw new Error('Invalid arguments, this function only accepts types of integer')
+    }
+    return ((a % n) + n) % n;
 }
 
 export {mod};
